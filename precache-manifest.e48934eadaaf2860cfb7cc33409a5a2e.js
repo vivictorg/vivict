fe131eaa4c7271605121d63d9509dd4d68112481
Gustav Grusell <gustav.grusell@svt.se>
2019-05-03 10:59:01 +0200

    Updates

---
+++ precache-manifest.e48934eadaaf2860cfb7cc33409a5a2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e2c8a237ecf054d68024",
+    "url": "./static/js/main.06f71b1f.chunk.js"
+  },
+  {
+    "revision": "2432505f210f8ab61be1",
+    "url": "./static/js/2.91bcd004.chunk.js"
+  },
+  {
+    "revision": "e2c8a237ecf054d68024",
+    "url": "./static/css/main.36208a89.chunk.css"
+  },
+  {
+    "revision": "2432505f210f8ab61be1",
+    "url": "./static/css/2.c2faae76.chunk.css"
+  },
+  {
+    "revision": "55d60236396036237024569cdbd12f9a",
+    "url": "./index.html"
+  }
+];
